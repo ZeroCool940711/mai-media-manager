ca434526423d5dc9bcbbf335178004ca3a1b988a
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2024-02-05 14:43:58 -0700

    ...

--- src/mai_media_manager/utils.py
+++ src/mai_media_manager/utils.py
@@ -1,30 +1,36 @@
 import os
-from whoosh import index
+
 from config.db.Model import Options
+from whoosh import index
 
 if not os.path.exists("db"):
     os.mkdir("db")
-    
+
+
 def first_run():
-    if not check_if_index_exists("db",'options'):
+    if not check_if_index_exists("db", "options"):
         set_default_config()
         return True
     else:
         return False
-    
+
+
 def create_index(schema, indexname):
     # Using the convenience functions
     ix = index.create_in("db", schema=schema, indexname=indexname)
     return ix
 
+
 def open_index(dirname, indexname, schema):
     # Using the convenience functions
     ix = index.open_dir(dirname=dirname, indexname=indexname, schema=schema)
     return ix
-    
+
+
 def check_if_index_exists(dirname, indexname):
     return index.exists_in(dirname, indexname=indexname)
-    
+
+
 def set_default_config(reset=False):
     """
     Set default config for the app. We will use this to create the index for the first time.
@@ -39,17 +45,18 @@ def set_default_config(reset=False):
         "automatic_updates_interval": 12,
     }
     if reset:
-        ix = create_index(Options, "options")
-        
-    if check_if_index_exists("db", "options"):        
+        ix = create_index(Options, "options")  # type: ignore
+
+    if check_if_index_exists("db", "options"):
         ix = index.open_dir("db", indexname="options", schema=Options)
     else:
         ix = create_index(Options, "options")
-        
+
     writer = ix.writer()
     writer.add_document(**defaults)
     writer.commit(merge=True, optimize=True)
-    
+
+
 def get_options(dirname="db", indexname="options", last_version=True):
     """
     Retrieve options from the specified index.
@@ -72,8 +79,9 @@ def get_options(dirname="db", indexname="options", last_version=True):
         else:
             # return all versions
             return list(results)
-        
-#defaults = set_default_config()
-#print (first_run())
-#print (get_options(last_version=True))
-#print(check_if_index_exists("db", "options"))
+
+
+# defaults = set_default_config()
+# print (first_run())
+# print (get_options(last_version=True))
+# print(check_if_index_exists("db", "options"))
