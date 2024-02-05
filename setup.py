d3afe9b868a3c626abfddcc405951aac082e9880
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2024-02-05 14:54:55 -0700

    ...

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from runpy import run_path
 from setuptools import find_packages, setup
 
 # read the program version from version.py (without loading the module)
-__version__ = run_path('src/mai_media_manager/version.py')['__version__']
+#__version__ = run_path('src/mai/version.py')['__version__']
+import versioneer
 
 
 def read(fname):
@@ -13,16 +14,16 @@ def read(fname):
 
 
 setup(
-    name="mai-media-manager",
-    version=__version__,
-    author="Alejandro M Gil Elias",
+    name="mai",
+    version=versioneer.get_version(),
+    author="Alejandro Gil",
     author_email="alejandrogilelias940711@gmail.com",
-    description="",
-    license="MIT",
+    description="A short summary of the project",
+    license="proprietary",
     url="",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    package_data={'mai_media_manager': ['res/*']},
+    package_data={'mai': ['res/*']},
     long_description=read('README.md'),
     install_requires=[],
     tests_require=[
@@ -32,4 +33,5 @@ setup(
     ],
     platforms='any',
     python_requires='>=3.8',
+    cmdclass=versioneer.get_cmdclass(),
 )
---
+++ src/mai/__init__.py
@@ -0,0 +1,9 @@
+"""
+Mai
+
+A short summary of the project
+"""
+__title__ = "mai"
+__copyright__ = "© 2024 Sygil-Dev"
+
+from ._version import __version__
